66a29e6885fe115f062c9c86576365844672b309
Olle Holmberg <olleholm91@hotmail.com>
2018-07-23 11:07:59 +0200

    Add files via upload

---
+++ hh
@@ -0,0 +1,16 @@
+
+THE ROCERD RAW BAYTES HAVE:(?,)
+record bytes:::::  Tensor("Reshape:0", shape=(512, 512, 3), dtype=uint8)
+SHAPE OF TENSOR IS: (?, ?, 3)
+Filling queue with 200 DRD images before starting to train. This will take a few minutes.
+THE ROCERD RAW BAYTES HAVE:(?,)
+record bytes:::::  Tensor("Reshape_2:0", shape=(512, 512, 3), dtype=uint8)
+
+/home/olle/anaconda3/envs/tensorflow/bin/python /home/olle/PycharmProjects/kaggle_drd/drd_train_resnet_v1_30.py
+the filename queue is <tensorflow.python.ops.data_flow_ops.FIFOQueue object at 0x7fd4630d1e50>
+THE ROCERD RAW BAYTES HAVE:(?,)
+record bytes:::::  Tensor("Reshape:0", shape=(512, 512, 3), dtype=uint8)
+SHAPE OF TENSOR IS: (?, ?, 3)
+Filling queue with 200 DRD images before starting to train. This will take a few minutes.
+THE ROCERD RAW BAYTES HAVE:(?,)
+record bytes:::::  Tensor("Reshape_2:0", shape=(512, 512, 3), dtype=uint8)
---
+++ inspect_tfrecords.py
@@ -0,0 +1,69 @@
+
+import os
+import tensorflow as tf
+
+IMAGE_SIZE = 462
+data_dir = '/home/olle/PycharmProjects/Diabetic_Retinopathy_Detection/create_tfrecords/data/'
+batch_size = 1
+# Global constants describing the Diabetic Retinopath Detection data set.
+NUM_CLASSES = 5
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 500 # was set from # 50000
+NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 3500
+CAPACITY = 200 #number of elements to queue
+
+
+
+filenames = [os.path.join(data_dir, 'data_batch_%d.bin' % i)
+             for i in range(1, len(os.listdir(data_dir)) - 1)]
+for f in filenames:
+    if not tf.gfile.Exists(f):
+        raise ValueError('Failed to find file: ' + f)
+
+# #sppecifying angles for images to be rotated by
+# number_of_samples =
+
+# Create a queue that produces the filenames to read.
+filename_queue = tf.train.string_input_producer(filenames)
+# Read examples from files in the filename queue
+print("the filename queue is {}".format(filename_queue))
+
+
+class SVHNRecord(object):
+    pass
+
+
+result = SVHNRecord()
+
+# Dimensions of the images in the SVHN dataset.
+# See http://ufldl.stanford.edu/housenumbers/ for a description of the
+# input format.
+result.height = 512
+result.width = 512
+result.depth = 3
+
+reader = tf.TFRecordReader()
+result.key, value = reader.read(filename_queue)
+value = tf.parse_single_example(
+    value,
+    # Defaults are not specified since both keys are required.
+    features={
+        'image_raw': tf.FixedLenFeature(shape=[], dtype=tf.string),
+        'label': tf.FixedLenFeature(shape=[], dtype=tf.int64)
+    })
+
+# Convert from a string to a vector of uint8 that is record_bytes long.
+record_bytes = tf.decode_raw(value['image_raw'], tf.uint8)
+#print("THE ROCERD RAW BAYTES HAVE:{}".format(record_bytes.get_shape()))
+record_bytes = tf.reshape(record_bytes, [result.height, result.width, 3])
+# record_bytes.set_shape([32*32*3])
+# # Build an initialization operation to run below.
+init = tf.global_variables_initializer()
+# Start running operations on the Graph.
+sess = tf.Session()
+# sess.run(init)
+
+# Start the queue runners.
+tf.train.start_queue_runners(sess=sess)
+
+im = sess.run(record_bytes)
+print(im.shape)
